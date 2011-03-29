42180eaf2d9515ad33a79521279e6700e9dfc156
leechuck2@googlemail.com <leechuck2@googlemail.com@cf77759e-dc04-357e-c137-c863623c0a50>
2011-03-29 09:11:39 +0000

    initial import

---
+++ src/uk/ac/ebi/owlapi/extension/ManIntersectionOfHandler.java
@@ -0,0 +1,124 @@
+package uk.ac.ebi.owlapi.extension;
+
+import org.coode.owlapi.manchesterowlsyntax.*;
+import org.coode.owlapi.obo.parser.* ;
+import org.semanticweb.owlapi.model.* ;
+import org.semanticweb.owlapi.expression.* ;
+import java.util.* ;
+
+public class ManIntersectionOfHandler extends AbstractTagValueHandler {
+
+    HashMap<String, ManOBORelation> relations = null ;
+
+    public ManIntersectionOfHandler(OBOConsumer consumer, HashMap<String, ManOBORelation>relations) {
+        super("intersection_of", consumer);
+	this.relations = relations ;
+    }
+
+    private OWLClassExpression getNewOWLClassOrRestriction(String termList) {
+	StringTokenizer tok = new StringTokenizer(termList, " ", false);
+        String id0 = null;
+        String id1 = null;
+        id0 = tok.nextToken();
+        if (tok.hasMoreTokens()) {
+            id1 = tok.nextToken();
+        }
+        if (id1 == null) {
+            return getDataFactory().getOWLClass(getIRIFromValue(id0));
+	    // Use standard pattern if no OWLDEF available; otherwise use OWLDEF pattern
+        } else {
+
+	    OWLClassExpression toClass = null ; 
+	    if (relations.containsKey(id0) && (relations.get(id0).getOwldef() != null)) {
+		
+		String namespace = null; 
+		if (getConsumer().getCurrentNamespace() == null) {
+		    namespace = getConsumer().getDefaultNamespace();
+		}	else {
+		    namespace = getConsumer().getCurrentNamespace();
+		}
+		String toTerm = id1.trim() ;
+		OWLEntityChecker checker = new StupidEntityChecker(getDataFactory(), namespace);
+		
+		OWLObjectProperty prop = getDataFactory().getOWLObjectProperty(getIRIFromValue(id0));
+		applyChange(new AddAxiom(getOntology(), getDataFactory().getOWLDeclarationAxiom(prop)));
+		
+		String s = relations.get(id0).getOwldef();
+		
+		while (s.indexOf("?Y")>=0) {
+		    String toTerm2 = toTerm.replaceAll(":","_") ;
+		    s = s.replace("?Y", toTerm2);
+		}
+		s = s.replaceAll("\"", "");
+		
+		ManchesterOWLSyntaxClassExpressionParser ms = new ManchesterOWLSyntaxClassExpressionParser(getDataFactory(), checker);
+		//          OWLAxiom axiom = ms.parse(s);
+		
+		try {
+		    toClass = ms.parse(s) ;
+		} catch (Exception E) {
+		    System.out.println ("Could not parse class expression "+s) ;
+		    E.printStackTrace() ;
+		}
+		return toClass ;
+	    } else {
+		OWLObjectProperty prop = getDataFactory().getOWLObjectProperty(getIRIFromValue(id0));
+		OWLClass filler = getDataFactory().getOWLClass(getIRIFromValue(id1));
+		return getDataFactory().getOWLObjectSomeValuesFrom(prop, filler);
+	    }
+	}
+	
+    }
+    
+    public void handle(String id, String value) {
+        getConsumer().addIntersectionOfOperand(getNewOWLClassOrRestriction(value)) ;
+    }
+
+    private static class StupidEntityChecker implements OWLEntityChecker {
+        private OWLDataFactory factory;
+        private String namespace;
+        
+        public StupidEntityChecker(OWLDataFactory factory, String _namespace) {
+            this.factory = factory;
+            this.namespace = _namespace;
+        }
+        
+
+
+        public OWLClass getOWLClass(String name) {
+            if (Character.isUpperCase(name.toCharArray()[0])) {
+                return factory.getOWLClass(IRI.create(namespace + name));
+            }
+            else {
+                return null;
+            }
+        }
+        
+        public OWLObjectProperty getOWLObjectProperty(String name) {
+            if (!Character.isUpperCase(name.toCharArray()[0])) {
+                return factory.getOWLObjectProperty(IRI.create(namespace + name));
+            }
+            else {
+                return null;
+            }
+        }
+        
+        public OWLAnnotationProperty getOWLAnnotationProperty(String name) {
+            return null;
+        }
+
+        public OWLDataProperty getOWLDataProperty(String name) {
+            return null;
+        }
+
+        public OWLDatatype getOWLDatatype(String name) {
+            return null;
+        }
+
+        public OWLNamedIndividual getOWLIndividual(String name) {
+            return null;
+        }
+        
+    }
+
+}
---
+++ src/uk/ac/ebi/owlapi/extension/ManOWLOBOParser.java
@@ -0,0 +1,124 @@
+package uk.ac.ebi.owlapi.extension;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.HashMap;
+
+import org.coode.owlapi.obo.parser.OBOConsumer;
+import org.coode.owlapi.obo.parser.OBOOntologyFormat;
+import org.coode.owlapi.obo.parser.OBOParser;
+import org.coode.owlapi.obo.parser.ParseException;
+import org.coode.owlapi.obo.parser.TokenMgrError;
+import org.semanticweb.owlapi.io.AbstractOWLParser;
+import org.semanticweb.owlapi.io.OWLOntologyDocumentSource;
+import org.semanticweb.owlapi.io.OWLParserException;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyFormat;
+
+
+public class ManOWLOBOParser extends AbstractOWLParser{
+	
+    public OWLOntologyFormat parse(OWLOntologyDocumentSource documentSource, OWLOntology ontology) throws OWLParserException, IOException {
+        OBOParser parser;
+        Reader reader;
+        HashMap<String, ManOBORelation> relations = null; 
+        
+        if (documentSource.isReaderAvailable()) {
+            parser = new OBOParser(documentSource.getReader());
+            reader = documentSource.getReader();
+        }
+        else if (documentSource.isInputStreamAvailable()) {
+            parser = new OBOParser(documentSource.getInputStream());
+            reader = new InputStreamReader(documentSource.getInputStream());
+        }
+        else {
+            parser = new OBOParser(getInputStream(documentSource.getDocumentIRI()));
+            reader = new InputStreamReader(getInputStream(documentSource.getDocumentIRI()));
+        }
+        
+        relations = readRelations(reader);
+        parser.setHandler(new ManOBOConsumer(getOWLOntologyManager(), ontology, relations));
+        
+        try {
+            parser.parse();
+        }
+        catch (ParseException e) {
+            throw new OWLParserException(e, e.currentToken.beginLine, e.currentToken.beginColumn);
+        }
+        catch(TokenMgrError e) {
+            throw new OWLParserException(e);
+        }
+        return new OBOOntologyFormat();
+    }
+	
+    public HashMap<String, ManOBORelation> readRelations(Reader _reader) {
+	HashMap<String, ManOBORelation> relations = new HashMap<String, ManOBORelation>();
+	BufferedReader br = new BufferedReader(_reader);
+	String fileData = null;
+		
+	// System.out.println("1");
+		
+	try {
+	    while ( (fileData = br.readLine()) != null) {
+				
+		if (fileData.equals("[Typedef]")) {
+					
+		    String id = null;
+		    String name = null;
+		    String owldef = null;
+		    String reldef = null;
+					
+		    while ( (fileData = br.readLine()) != null) {
+						
+				    	
+				    	
+			// System.out.println("2");
+						
+				    	
+			if ( fileData.startsWith("id: ") ) {
+			    id = fileData.substring(4).trim();
+			    // System.out.println("4");
+							
+			    if (id.indexOf("!") != -1) {
+				id = id.substring(0, id.indexOf("!")).trim();
+			    }
+			}
+						
+			if ( fileData.startsWith("name: ")) {
+			    //							System.out.println("3");
+			    name = fileData.substring(6).trim();			
+							
+			}
+						
+			if ( fileData.startsWith("owldef: ")) {
+			    // System.out.println("3");
+			    owldef = fileData.substring(8).trim();			
+									
+			    if ( id != null) {
+				relations.put(id, new ManOBORelation(id, name, owldef));
+			    }
+			}
+			if ( fileData.startsWith("reldef: ")) {
+			    // System.out.println("3");
+			    owldef = fileData.substring(8).trim();			
+			    
+			    if ( id != null) {
+				relations.put(id, new ManOBORelation(id, name, reldef));
+			    }
+			}
+							
+							
+		    }
+						
+		}
+	    }
+	} catch (IOException ioe) {
+	    ioe.printStackTrace();
+	}
+		
+	return relations;
+    }
+
+}
